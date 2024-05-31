9ac8a2d93d034e319b62a7f3d87a7d2967905531
Tiago Benaias Pompeo <tiapom@gmail.com>
2024-05-31 17:53:47 +0100

    Star Project

---
+++ MauiCleanPlayGround/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCleanPlayGround;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiCleanPlayGround/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCleanPlayGround;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
